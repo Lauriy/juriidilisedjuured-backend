3429c9d33c91be9682d96f8a7dd5a4d542394da0
lauri <lauri.elias@outlook.com>
2016-08-02 02:42:10 +0300

    Update server config, move static assets

--- production-uwsgi.sh
+++ production-uwsgi.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export HOME=/home/juriidilisedjuured
 cd $HOME/juriidilisedjuured-backend/
+source ~/.bashrc
 workon juriidilisedjuured-prod
+python manage.py collectstatic
 uwsgi --socket /home/juriidilisedjuured/juriidilisedjuured-backend/jj.sock --module juriidilisedjuured.wsgi --chmod-socket=777 --env=LANG="en_US.utf8"
 exit $?
