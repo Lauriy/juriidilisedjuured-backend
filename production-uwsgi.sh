0b367a0f9d5d0a21a56ec5049000e3d460302891
lauri <lauri.elias@outlook.com>
2016-08-02 02:50:32 +0300

    Try to fix supervisor

--- production-uwsgi.sh
+++ production-uwsgi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export HOME=/home/juriidilisedjuured
+cd $HOME
+source .virtualenvs/juriidilisedjuured-prod/bin/activate
 cd $HOME/juriidilisedjuured-backend/
-source ~/.bashrc
-workon juriidilisedjuured-prod
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 uwsgi --socket /home/juriidilisedjuured/juriidilisedjuured-backend/jj.sock --module juriidilisedjuured.wsgi --chmod-socket=777 --env=LANG="en_US.utf8"
 exit $?
